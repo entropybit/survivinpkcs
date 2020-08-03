d405c349a0d73f512ba7851a1b9c25bf85552673
Benjamin Mayer <benjamin.e.mayer@gmail.com>
2020-08-03 02:48:03 +0200

    docking scripts

---
+++ global_docking.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+ROSETTA_PATH=/opt/rosetta_src_2018.33.60351_bundle
+FOLDER=$HOME/nfs/DNAPKcs
+RS_EXEC=$ROSETTA_PATH/main/source/bin
+RS_DATA=$ROSETTA_PATH/main/database
+NCORES=48
+N=100000
+PREPACK=docking_prepack_protocol.mpi.linuxgccrelease
+DOCK=docking_protocol.mpi.linuxgccrelease
+
+DOCK_DIMER=true
+DOCK_MONOMER=false
+
+
+if ${DOCK_DIMER}; then
+    $RS_EXEC/$PREPACK -docking:partners BC_A -docking::sc_min -s $FOLDER/pk_mono_sur_di.pdb -database $RS_DATA
+    mkdir $FOLDER/sur_dimer
+    mv $FOLDER/pk_mono_sur_di_0001.pdb $FOLDER/sur_dimer/
+    cd $FOLDER/sur_dimer
+    mpiexec --hostfile $FOLDER/hostfile -n $NCORES $RS_EXEC/$DOCK -docking:partners BC_A -randomize1 -randomize2 -spin -dock_pert 3 8 -low_res_protocol_only -nstruct $N -native $FOLDER/pk_mono_sur_di.pdb -s $FOLDER/sur_dimer/pk_mono_sur_di_0001.pdb -database $RS_DATA -score:docking_interface_score 1
+    cd $FOLDER
+fi
+
+if ${DOCK_MONOMER}; then
+    $RS_EXEC/$PREPACK -docking:partners C_A -docking::sc_min -s $FOLDER/pk_mono_sur_mono.pdb -database $RS_DATA
+    mkdir $FOLDER/sur_monomer
+    mv $FOLDER/pk_mono_sur_mono_0001.pdb $FOLDER/sur_monomer/
+    cd $FOLDER/sur_monomer
+    mpiexec --hostfile $FOLDER/hostfile -n $NCORES $RS_EXEC/$DOCK -docking:partners C_A -randomize1 -randomize2 -spin -dock_pert 3 8 -low_res_protocol_only -nstruct $N -native $FOLDER/pk_mono_sur_mono.pdb -s $FOLDER/sur_monomer/pk_mono_sur_mono_0001.pdb -database $RS_DATA -score:docking_interface_score 1
+    cd $FOLDER
+fi
---
+++ refinement_docking.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ROSETTA_PATH=/opt/rosetta_src_2018.33.60351_bundle
+FOLDER=$HOME/nfs/DNAPKcs
+RS_EXEC=$ROSETTA_PATH/main/source/bin
+RS_DATA=$ROSETTA_PATH/main/database
+NCORES=80
+N=1
+#PREPACK=docking_prepack_protocol.mpi.linuxgccrelease
+DOCK=docking_protocol.default.linuxgccrelease
+
+if true; then
+    mkdir $FOLDER/dimer_refined/
+    ls $FOLDER/sur_dimer | grep _0001_ | grep .pdb | parallel $RS_EXEC/$DOCK -docking:partners BC_A -docking_local_refine -nstruct 1 -database $RS_DATA -score:docking_interface_score 1 -native $FOLDER/pk_mono_sur_di.pdb --overwrite -out:prefix $FOLDER/dimer_refined/ -out:file:scorefile dimer_scores.sc -s $FOLDER/sur_dimer/{} ::: ${FILES[@]} 
+fi
+
+if false; then
+    mkdir $FOLDER/monomer_refined
+    ls $FOLDER/sur_monomer | grep _0001_ | grep .pdb | parallel $RS_EXEC/$DOCK -docking:partners C_A -docking_local_refine -nstruct 1 -database $RS_DATA -score:docking_interface_score 1 -native $FOLDER/pk_mono_sur_mono.pdb --overwrite -out:prefix $FOLDER/monomer_refined/ -out:file:scorefile monomer_scores.sc -s $FOLDER/sur_monomer/{} ::: ${FILES[@]}
+fi
